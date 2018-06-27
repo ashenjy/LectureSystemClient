2fe59eb1ed765bedd82ec52ce5fed3005c9ea529
Lahiru Tennakoon <lahiru.tennakoon@my.sliit.lk>
2018-06-28 00:47:23 +0530

    Create video chapters in location.

--- src/components/presentation/afterLogin/lecturer/LectureVideos_lt.js
+++ src/components/presentation/afterLogin/lecturer/LectureVideos_lt.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 
-// Import services
+// Import configuration file
+import { config } from "../../../../configurations/config";
+
+// Import methods from service
 import { getAllVideos } from "../../../../services/videosService_lt";
+import { createVideoChaptersService } from "../../../../services/videosService_lt";
 
 class LectureVideos_lt extends Component{
     constructor(props){
@@ -20,6 +24,29 @@ class LectureVideos_lt extends Component{
             this.setState({
                 videoList: data
             })
+        });
+
+        console.log('config');
+        console.log(config.videoSavingPath);
+    }
+
+    // Function to create video chapters
+    createVideoChapters(lectureVideo) {
+        console.log('clicked');
+        console.log(lectureVideo);
+
+        createVideoChaptersService(lectureVideo).then(data=> {
+            console.log('done');
+            console.log(data);
+
+            if(data.success === true)
+            {
+                alert("Success");
+            }
+            else
+            {
+                alert("Failed");
+            }
         })
     }
 
@@ -42,10 +69,18 @@ class LectureVideos_lt extends Component{
                             <tr>
                                 <td>{ video.subject }</td>
                                 <td>{ video.videoName }</td>
-                                <td>{ video.lectureVideo }</td>
+                                <td>
+                                    <video controls className="form-control" id="lectureVideoPreview" width="320"
+                                           height="280">
+                                        <source src="D:/tmp/1530122414128_goldeneye.mp4"/>
+                                            Your browser does not support HTML5 video.
+
+                                    </video>
+                                </td>
+                                {/*<td>{ video.lectureVideo }</td>*/}
                                 <td>{ video.dateTime.substring(4, 24) }</td>
                                 <td>
-                                    <button type="button" className="btn btn-primary">Create Chapters</button>
+                                    <button type="button" className="btn btn-primary" onClick={() => this.createVideoChapters(video.lectureVideo)}>Create Chapters</button>
                                 </td>
                                 <td>{ video.status }</td>
                             </tr>
--- src/services/videosService_lt.js
+++ src/services/videosService_lt.js
@@ -5,3 +5,9 @@ export function getAllVideos() {
         .then(res => res.data)
         .catch(error => error);
 }
+
+export function createVideoChaptersService(lectureVideo) {
+    return axios.post('/api/videoChapters', {lectureVideo: lectureVideo})
+        .then(res => res.data)
+        .catch(error => error);
+}
