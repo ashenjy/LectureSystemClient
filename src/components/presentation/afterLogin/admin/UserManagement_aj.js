af2911bd5a389967cb8a43049e3c0f9cc9d424d6
ashen <jayasinghe.ashen@gmail.com>
2018-08-01 16:47:17 +0530

    session and route fixed

--- src/components/presentation/afterLogin/admin/UserManagement_aj.js
+++ src/components/presentation/afterLogin/admin/UserManagement_aj.js
@@ -1,14 +1,41 @@
 import React, {Component} from 'react';
 import { Link } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 
 class UserManagement_aj extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            redirect:false
+        };
+    }
+
+    componentWillMount() {
+        if (localStorage.getItem("userid")) {
+
+        }
+        else {
+            this.setState({ redirect: true });
+        }
+    }
+
     render() {
+
+        if(this.state.redirect){
+            return(<Redirect to={'/loginselection'}/>)
+        }
+
+        if (localStorage.getItem("userid")) {
+            var userId = localStorage.getItem('userid');
+        }
+
         return (
             <div>
                 <h3>User Management</h3>
                 <ol className="breadcrumb">
-                    <li><Link to={'/loginselection/userManagement/viewUser'}>View User</Link></li>
-                    <li><Link to={'/loginselection/userManagement/registerUser'}>Register User</Link></li>
+                    <li><Link to={'/userManagement/viewUser/'+userId}>View User</Link></li>
+                    <li><Link to={'/userManagement/registerUser/'+userId}>Register User</Link></li>
                 </ol>
 
                 <div>
--- src/components/presentation/afterLogin/lecturer/MainView_vm.js
+++ src/components/presentation/afterLogin/lecturer/MainView_vm.js
@@ -1,8 +1,31 @@
 import React, {Component} from 'react';
 import { Link } from 'react-router-dom';
+import {Redirect} from 'react-router-dom';
 
 class MainView_vm extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            redirect:false
+        };
+
+    }
+
+    componentWillMount() {
+        if (localStorage.getItem("userid")) {
+
+        }
+        else {
+            this.setState({ redirect: true });
+        }
+    }
+
     render() {
+        if(this.state.redirect){
+            return(<Redirect to={'/loginselection'}/>)
+        }
+
         return (
             <div>
                 <h3>Main View</h3>
