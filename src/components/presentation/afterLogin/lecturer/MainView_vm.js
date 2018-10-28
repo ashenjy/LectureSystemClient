e1191ef31ac7b79b6f9e9c44cfb986b7d00b669d
Vimukthi Mudalige <vimukthimudalige@gmail.com>
2018-10-28 15:57:40 +0530

    updated interface buttons for manual camera movement

--- src/components/presentation/afterLogin/lecturer/MainView_vm.js
+++ src/components/presentation/afterLogin/lecturer/MainView_vm.js
@@ -3,7 +3,9 @@ import { Link } from 'react-router-dom';
 import {Redirect} from 'react-router-dom';
 
 // Import services
-import { recalibrateCamera } from "../../../../services/ptzService_vm";
+import {recalibrateCamera, turnDownCamera, turnRightCamera, turnUpCamera} from "../../../../services/ptzService_vm";
+import { turnLeftCamera } from "../../../../services/ptzService_vm";
+import { stopMovementCamera } from "../../../../services/ptzService_vm";
 import {createVideoChaptersService} from "../../../../services/videosService_lt";
 
 class MainView_vm extends Component {
@@ -26,12 +28,9 @@ class MainView_vm extends Component {
     }
 
     recalibrateCamera() {
-        console.log('ok');
-
         recalibrateCamera().then(data=> {
-            console.log('done');
-            console.log(data);
-
+            //console.log('done');
+            //console.log(data);
             if(data.success === true)
             {
                 alert("Success");
@@ -43,6 +42,83 @@ class MainView_vm extends Component {
         })
     }
 
+    turnLeftCamera() {
+        turnLeftCamera().then(data=> {
+            //console.log('done');
+            console.log(data);
+            if(data.success === true)
+            {
+                alert("Success");
+            }
+            else
+            {
+                alert("Failed");
+            }
+        })
+    }
+
+    turnRightCamera() {
+        turnRightCamera().then(data=> {
+            //console.log('done');
+            console.log(data);
+            if(data.success === true)
+            {
+                //alert("Success");
+            }
+            else
+            {
+                alert("Failed");
+            }
+        })
+    }
+
+    turnUpCamera() {
+        turnUpCamera().then(data=> {
+            //console.log('done');
+            console.log(data);
+            if(data.success === true)
+            {
+                //alert("Success");
+            }
+            else
+            {
+                alert("Failed");
+            }
+        })
+    }
+
+    turnDownCamera() {
+        turnDownCamera().then(data=> {
+            //console.log('done');
+            console.log(data);
+            if(data.success === true)
+            {
+                //alert("Success");
+            }
+            else
+            {
+                alert("Failed");
+            }
+        })
+    }
+
+    stopMovementCamera() {
+        stopMovementCamera().then(data=> {
+            //console.log('done');
+            console.log(data);
+            if(data.success === true)
+            {
+                alert("Success");
+            }
+            else
+            {
+                alert("Failed");
+            }
+        })
+    }
+
+
+
     render() {
         if(this.state.redirect){
             return(<Redirect to={'/'}/>)
@@ -50,7 +126,7 @@ class MainView_vm extends Component {
 
         return (
             <div>
-                <h3>Main View THIS</h3>
+                <h3>Advanced Controls</h3>
 
                 <div className="row">
                     <div className="col s6 center">
@@ -58,48 +134,69 @@ class MainView_vm extends Component {
                             <tr>
                                 <td></td>
                                 <td>
-                                    <button id="btnMoveUp" className="btn btn-info">Up</button>
+                                  <button id="btnMoveUp" className="btn btn-primary" onClick={this.turnUpCamera}>  Up</button>
                                 </td>
                                 <td></td>
                             </tr>
                             <tr>
                                 <td>
-                                    <button id="btnMoveLeft" className="btn btn-info">Left</button>
+                                    <button id="btnMoveLeft" className="btn btn-primary" onClick={this.turnLeftCamera}>Left</button>
                                 </td>
                                 <td>
 
                                 </td>
                                 <td>
-                                    <button id="btnMoveRight" className="btn btn-info">Right</button>
+                                    <button id="btnMoveRight" className="btn btn-primary" onClick={this.turnRightCamera}>Right</button>
                                 </td>
                             </tr>
                             <tr>
                                 <td></td>
                                 <td>
-                                    <button id="btnMoveDown" className="btn btn-info">Down</button>
+                                    <button id="btnMoveDown" className="btn btn-primary" onClick={this.turnDownCamera}>Down</button>
                                 </td>
                                 <td></td>
+
+
                             </tr>
+
+
                         </table>
                     </div>
 
                     <br/><br/>
 
+
+                    <div className="col s6 center">
+                    <td></td>
+                    <td>
+                        <button id="btnMoveDown" className="btn btn-warning" onClick={this.turnDownCamera}>Zoom In</button> &nbsp;&nbsp;
+                    </td>
+                    <td></td>
+
+                    <td></td>
+                    <td>
+                        <button id="btnMoveDown" className="btn btn-warning" onClick={this.turnDownCamera}>Zoom Out</button> &nbsp;&nbsp;
+                    </td>
+                    <td></td>
+                    </div>
+                    <br/><br/>
                     <div className="col s6 center">
                         <button id="btnRefresh" className="btn btn-success"
                                 onClick={this.recalibrateCamera}>Recalibrate</button>&nbsp;&nbsp;
                         <button id="btnRefresh" className="btn btn-success">Turn to Audience</button>&nbsp;&nbsp;
                         <button id="btnRefresh" className="btn btn-success">Turn Back to Lecturer</button>&nbsp;&nbsp;
-                        <button id="btnClear" className="btn btn-danger">Stop</button>
+                        <button id="btnClear" className="btn btn-danger" onClick={this.stopMovementCamera}>Stop</button>
                     </div>
+
+
 
                     <br/>
                     <br/>
                     <div className="col s6 center">
                         <button id="btnRefresh" className="btn btn-success">Start Lecturer Tracker</button>&nbsp;&nbsp;
                         <button id="btnRefresh" className="btn btn-success">Start Gesture Detection</button>&nbsp;&nbsp;
-                        <button id="btnRefresh" className="btn btn-success">Stop Tracker</button>&nbsp;&nbsp;
-                        <button id="btnClear" className="btn btn-danger">Clear</button>
+                        <button id="btnRefresh" className="btn btn-danger">Stop Tracker</button>&nbsp;&nbsp;
+                        <button id="btnClear" className="btn btn-info">Clear</button>
                     </div>
                 </div>
             </div>
--- src/services/ptzService_vm.js
+++ src/services/ptzService_vm.js
@@ -5,3 +5,33 @@ export function recalibrateCamera() {
         .then(res => res.data)
         .catch(error => error);
 }
+
+export function turnLeftCamera() {
+    return axios.get('/api/turnLeftCamera')
+        .then(res => res.data)
+        .catch(error => error);
+}
+
+export function turnRightCamera() {
+    return axios.get('/api/turnRightCamera')
+        .then(res => res.data)
+        .catch(error => error);
+}
+
+export function turnUpCamera() {
+    return axios.get('/api/turnUpCamera')
+        .then(res => res.data)
+        .catch(error => error);
+}
+
+export function turnDownCamera() {
+    return axios.get('/api/turnDownCamera')
+        .then(res => res.data)
+        .catch(error => error);
+}
+
+export function stopMovementCamera() {
+    return axios.get('/api/stopMovementCamera')
+        .then(res => res.data)
+        .catch(error => error);
+}
